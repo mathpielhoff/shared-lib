c4dcf38ecce2e7c15e398f375292b0f3517fdc23
peyah <math.pielhoff@gmail.com>
2024-11-12 16:04:59 +0100

    fixing name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="shared_lib",
     version="0.1.0",
-    description="Shared configuration module for microservices",
+    description="Shared librairies module for microservices",
     author="mpielhoff",
     packages=find_packages(),
     install_requires=[
