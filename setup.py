0b8217eeee4ceb88582f221313006d8fdcf38f5c
peyah <math.pielhoff@gmail.com>
2024-11-12 15:58:09 +0100

    fix name

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="shared-config",
+    name="shared_lib",
     version="0.1.0",
     description="Shared configuration module for microservices",
     author="mpielhoff",
-    author_email="your.email@example.com",
     packages=find_packages(),
     install_requires=[
         "python-dotenv>=0.21.0",
