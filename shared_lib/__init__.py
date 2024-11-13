2d98fbc719e2a9fcb92a280b8c174e40801fb87b
peyah <math.pielhoff@gmail.com>
2024-11-13 15:23:10 +0100

    add base

--- shared_lib/__init__.py
+++ shared_lib/__init__.py
@@ -1,2 +1,2 @@
-from .utils import create_access_token, decode_jwt, config, get_db, init_db
+from .utils import create_access_token, decode_jwt, config, get_db, init_db, Base
 from .models.LoginResponse import LoginResponse
--- shared_lib/utils/__init__.py
+++ shared_lib/utils/__init__.py
@@ -1,3 +1,3 @@
 from .jwt_utils import decode_jwt, create_access_token
-from .db_utils import get_db, init_db
+from .db_utils import get_db, init_db, Base
 from .config import Config
